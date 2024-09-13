b529bd20383f7321272090f0f1a11ce788d45cb4
edmaker89 <edmaker@gmail.com>
2024-09-13 17:10:18 -0300

    link repository

--- setup.py
+++ setup.py
@@ -6,16 +6,16 @@ with open("README.md", "r") as f:
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
     
-# setup(
-#     name="validador_docs",
-#     version="0.0.1",
-#     author='edmaker89',
-#     author_email="edmaker@gmail.com",
-#     description="Description package",
-#     long_description=page_description,
-#     long_description_content_type="text/markdown",
-#     url="https://github.com/edmaker89/package-repository",
-#     packages=find_packages(),
-#     install_requires=requirements,
-#     python_requires=">=3.12"
-# )
+setup(
+    name="validador_docs",
+    version="0.0.1",
+    author='edmaker89',
+    author_email="edmaker@gmail.com",
+    description="Description package",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/edmaker89/validator_documents_package",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires=">=3.12"
+)
