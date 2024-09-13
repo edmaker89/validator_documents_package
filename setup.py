fc532cbe56489fab421f96def7b29fa9d3c43737
edmaker89 <edmaker@gmail.com>
2024-09-13 17:06:21 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+    
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+    
+# setup(
+#     name="validador_docs",
+#     version="0.0.1",
+#     author='edmaker89',
+#     author_email="edmaker@gmail.com",
+#     description="Description package",
+#     long_description=page_description,
+#     long_description_content_type="text/markdown",
+#     url="https://github.com/edmaker89/package-repository",
+#     packages=find_packages(),
+#     install_requires=requirements,
+#     python_requires=">=3.12"
+# )
